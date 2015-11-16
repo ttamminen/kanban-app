e7231989a67f4e7c5cd24e039da2ae747de95428
Tatu Tamminen <ttamminen@gmail.com>
2015-11-16 20:31:52 +0200

    adds lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,61 +1,26 @@
-import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // We have to bind the context of `storeChanged`
-    // explicitly so that `this` will point at the `App`
-    // instance. You'll be seeing this pattern a lot.
-    this.storeChanged = this.storeChanged.bind(this);
-    this.state = NoteStore.getState();
-  } 
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged(state) {
-    // Without proper `bind`, `this` wouldn't
-    // point at the right context (defaults to `window`
-    // in browser environment)
-    this.setState(state);
-  }
-
   render() {
-    const notes = this.state.notes;
-
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+        <button className="add-lane" onClick={this.addItem}>+</button>
+        <AltContainer
+          stores={[LaneStore]}
+          inject={{
+            items: () => LaneStore.getState().lanes || []
+          }}
+        >
+          <Lanes />
+        </AltContainer>
       </div>
     );
   }
-
-  addNote() {
-    NoteActions.create({task: 'New task'});
+  addItem() {
+    LaneActions.create({name: 'New lane'});
   }
-  editNote(id, task) {
-    NoteActions.update({id, task});
-  }
-  deleteNote(id) {
-    NoteActions.delete(id);
-  }
-  findNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = notes.findIndex((note) => note.id === id);
-
-    if(noteIndex < 0) {
-      console.warn('Failed to find note', notes, is);
-    }
-
-    return noteIndex;
-  }  
 }
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -41,6 +41,16 @@ class NoteStore {
       notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
     });
   }
+  findNote(id) {
+    const notes = this.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if(noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+
+    return noteIndex;
+  }
 }
 
 export default alt.createStore(NoteStore, 'NoteStore');
